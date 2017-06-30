38261fdac5372a4181bcb75bba2d04a7e9221612
Jared Planter <jaredplanter@gmail.com>
2017-06-29 21:34:03 -0700

    Update User#points_balance
    To return 0 if there are no point ledger entries
    
    Includes spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   has_many :point_ledger_entries, through: :purchases
 
   def points_balance
-    @points_balance ||= point_ledger_entries.last.balance
+    @points_balance = if last_point_ledger_entry.present?
+                        last_point_ledger_entry.balance
+                      else
+                        0
+                      end
+  end
+
+  private
+
+  def last_point_ledger_entry
+    point_ledger_entries.last
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:point_ledger_entries) { [double(balance: 1)] }
+  subject(:user) { User.new }
+
+  describe '#points_balance' do
+    context 'when the point ledger is empty' do
+      it 'it returns 0' do
+        allow(user).to receive(:point_ledger_entries).and_return []
+        expect(user.points_balance).to eq 0
+      end
+    end
+    context 'when the point ledger is not empty' do
+      it 'returns the current balance' do
+        allow(user).to receive(:point_ledger_entries).and_return point_ledger_entries
+        expect(user.points_balance).to eq 1
+      end
+    end
+  end
 end
