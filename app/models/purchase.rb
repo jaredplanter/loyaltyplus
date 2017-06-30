43b04db8b14cfa1bb0917baaf8af05e6a963b7f1
Jared Planter <jaredplanter@gmail.com>
2017-06-29 21:23:56 -0700

    Add Purchase#points_remaining
    To calculate how many points remain from the amount of points earned
    for the current purchase
    
    Spec included

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,23 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   has_many :point_ledger_entries, dependent: :destroy
+
+  def points_remaining
+    if point_ledger[:debits] > point_ledger[:credits]
+      0
+    else
+      point_ledger[:credits] - point_ledger[:debits]
+    end
+  end
+
+  private
+
+  def point_ledger
+    credits_and_debits = {credits:0, debits:0}
+    point_ledger_entries.each do |ledger_entry|
+      credits_and_debits[:credits] += ledger_entry.credit
+      credits_and_debits[:debits] += ledger_entry.debit
+    end
+    credits_and_debits
+  end
 end
