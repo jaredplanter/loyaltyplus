b501a2f6491c058ac9e89570a266bcc213a55612
Jared Planter <jaredplanter@gmail.com>
2017-06-29 18:03:32 -0700

    Change amount default on Purchase to 0 (integer)
    To avoid undefined method errors when amount is added or subtracted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629190552) do
+ActiveRecord::Schema.define(version: 20170629211014) do
 
   create_table "point_ledger_entries", force: :cascade do |t|
     t.integer  "purchase_id"
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20170629190552) do
 
   create_table "purchases", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "amount",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "users", force: :cascade do |t|
