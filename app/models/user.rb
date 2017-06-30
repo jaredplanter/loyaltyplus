e30003c7cbc250501893035e4432be410eb094b0
Jared Planter <jaredplanter@gmail.com>
2017-06-29 18:04:06 -0700

    Rename User#point_balance to User#points_balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :purchases
   has_many :point_ledger_entries, through: :purchases
 
-  def point_balance
-    @point_balance ||= point_ledger_entries.last.balance
+  def points_balance
+    @points_balance ||= point_ledger_entries.last.balance
   end
 end
